5aafc32673fc1ff52864bdf6fc4c821d0547f28a
KristinDiep <kristiyip@gmail.com>
2018-02-16 16:24:05 -0500

    back button added

--- src/ExperimentPreview.js
+++ src/ExperimentPreview.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
+import Button from 'material-ui/Button';
+
+const buttonStyle = {
+	left: '39.5%',
+	margin: '2px',
+	boxShadow: '0 2px 2px 0 rgba(0, 0, 0, 0.14)',
+	backgroundColor: 'orange'
+}
 
 export default class ExperimentPreview extends React.Component {
 
@@ -11,6 +19,7 @@ export default class ExperimentPreview extends React.Component {
 		.then(json => {
 			for(var i=0; i<json.plugins.length; i++) {
 				const script = document.createElement("script");
+				script.className = "rendered-script";
 				script.src=json.plugins[i];
 				script.async=false;
 				document.body.appendChild(script);
@@ -19,18 +28,23 @@ export default class ExperimentPreview extends React.Component {
 			const link = document.createElement("link");
 			link.rel="stylesheet";
 			link.href=json.styling[0];
+			link.className = "rendered-script";
 			document.body.appendChild(link);
 		});
 	}
 
 	componentWillUnmount() {
-		
+		var toRemove = document.querySelector('.rendered-script');
+		for(var i=0; i<toRemove.length; i++){
+			toRemove[i].remove();
+		}
 	}
 	
 	render() {
 		return(
-			<div id="experimentContainer" style={{position: 'absolute', top: '5%', left: '5%', width: '90vw', height: '90vh'}}>
-				<Paper elevation={6} style={{width: '90vw', height: '90vh'}}>
+			<div id="experimentContainer" style={{width: '100vw', height: '100vh', backgroundColor: '#ddd'}}>
+				<Button variant="raised" color="black" style={buttonStyle}>Back To Home</Button>
+				<Paper elevation={6} style={{position: 'absolute', top: '5%', left: '5%', width: '90vw', height: '90vh'}}>
 					<div id="preview" style={{width: '90vw', height: '90vh'}}>
 					</div>
 				</Paper>
