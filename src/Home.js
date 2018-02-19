ae3d801ebca945ba98dbead5b72e4ea91ec013d7
KristinDiep <kristiyip@gmail.com>
2018-02-19 12:12:23 -0500

    list style implemented for home page

--- src/Home.js
+++ src/Home.js
@@ -2,24 +2,48 @@ import React from 'react';
 import { Link, Route, Switch} from 'react-router-dom';
 import LabNames from './LabNames';
 import LabPages from './LabPages';
+import GridList, { GridListTile } from 'material-ui/GridList';
+import List, { ListItem, ListItemText } from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
+
+const listStyle = {
+	list: {
+		width: '100%'
+	}
+}
 
 export default class Home extends React.Component {
 	render() {
 		return(
 			<div id="labs">
-				<div style={{width: '30%'}}>
-					<ul style={{ listStyleType: 'none', padding: 0 }}>
-			   			{
-			     			LabNames.all().map(lab => (
-								<li key={lab.index}>
-									<Link to={`/LabList/${lab.path}`}>{lab.name}</Link>
-								</li>
-							))
-			  			}
-			   		</ul>
+				<div>
+					<List style={listStyle.list}>
+					<Link to="/LabList/iat" style={{textDecoration: 'none'}}>
+						<ListItem button>
+							<Avatar alt="Image" src="blue.png" />
+							<ListItemText primary="IAT" secondary="Implicit Association Test contains images or HTML-formatted content as stimulus." />
+						</ListItem>
+					</Link>
+					<Link to="/LabList/flanker" style={{textDecoration: 'none'}}>
+						<ListItem button>
+							<Avatar alt="Flanker" src="blue.png" />
+							<ListItemText primary="Flanker" secondary="Tests reflex." />
+						</ListItem>
+					</Link>
+					</List>
 				</div>
 				<Route path='/LabList/:path' component={LabPages} />
 			</div>
 		)
 	}
 }
+
+// <ul style={{ listStyleType: 'none', padding: 0 }}>
+// 			   			{
+// 			     			LabNames.all().map(lab => (
+// 								<li key={lab.index}>
+// 									<Link to={`/LabList/${lab.path}`}>{lab.name}</Link>
+// 								</li>
+// 							))
+// 			  			}
+// 			   		</ul>
